5def8e18379cf424f12f763652502a84d5dd57a7
bld_jmsmachado <joaomiguelmachado@boldint.com>
2018-01-12 15:33:37 +0000

    PM: add Webclient spring integration tests
    Change-Id: Ifee76aeeb3841bd05d34c2e38443f2dd8d26ac0d

--- src/test/java/com/perfectmatch/controller/MatchWebClientTests.java
+++ src/test/java/com/perfectmatch/controller/MatchWebClientTests.java
@@ -6,8 +6,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.perfectmatch.persistence.model.Match;
+
+import reactor.core.publisher.Flux;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,11 +26,27 @@ public class MatchWebClientTests {
 	//from https://www.callicoder.com/spring-5-reactive-webclient-webtestclient-examples/
 	//fix SSL test https://github.com/netty/netty/blob/add7efc2d4f818bfb02cef7a34c701827e83ddf7/handler-proxy/src/test/java/io/netty/handler/proxy/ProxyHandlerTest.java#L284
     @Test
-    public void test5DeleteGithubRepository() {
+    public void testIfServerIsOk() {
         webTestClient.get()
                 .uri("/match/repo")
                 .exchange()
                 .expectStatus().isOk();
     }
+    
+    
+    @Test
+    public void testReadrepository() {
+    	//given
+    	//
+        webTestClient.get()
+                .uri("/match/repo")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Match.class)
+                .hasSize(1);
+                
+    }
+    
+    
 	
 }
